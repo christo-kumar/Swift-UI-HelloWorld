ec71498818c0edc352daf07404e7b3a72f003591
christo <christoslimit@gmail.com>
2022-06-28 15:49:28 +0530

    Swift UI Project Created

---
+++ Characters/Characters/CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersApp.swift
+//  Characters
+//
+//  Created by Christo Kumar on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
